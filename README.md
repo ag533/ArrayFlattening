7d1859ca4e8d39c11563f6420b4dd30249224fef
Abhishek GAhlawat <abhishekgahlawatoo7@gmail.com>
2021-12-03 12:10:04 +0000

    Added Tests as assert statements.

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # ArrayFlattening
 This repository holds the solution to flatten an array without using any pre defined flat function.
 
+## Problem Statment :
+Write an algorithm to flatten an arbitrarily nested array of values. 
+
+```
+[ 1, [ 2, [ 3 ] ], 4 ] -> [ 1, 2, 3, 4 ]
+```
+
+
+## Approach :
+
 As described in the question I am not supposed to use any inbuilt flattening function so I will try to create a recursive function that will call it self each time it found a nested array.
 
-
+## Testing :
+I have tried to added every possible test case.
+I am using deepStrictEqual rather than using equal as deepStrictEqual equates the type and value (===) of the result along with children type and value (===) which is not performed in case of equals(==);
--- array_flattening.js
+++ array_flattening.js
@@ -33,3 +33,15 @@ function flatten_array(input) {
         return flatArray;
     }
 }
+
+assert.deepStrictEqual(flatten_array([1,[2,[3]],4]),[1,2,3,4],"Test1 Fails");
+assert.deepStrictEqual(flatten_array(new Array([1])),[1],"Test1 Fails");
+assert.deepStrictEqual(flatten_array([]),[], "Test2 Fails");
+assert.deepStrictEqual(flatten_array([1,[]]),[1], "Test3 Fails");
+assert.deepStrictEqual(flatten_array([1,2,[3,[[[4,[5]]]]]]),[1,2,3,4,5], "Test4 Fails");
+assert.deepStrictEqual(flatten_array(213),'This function is for flattening array not any other datatype', "Test5 Fails");
+assert.deepStrictEqual(flatten_array({}),'This function is for flattening array not any other datatype', "Test6 Fails");
+assert.deepStrictEqual(flatten_array({123:123}),'This function is for flattening array not any other datatype', "Test7 Fails");
+assert.deepStrictEqual(flatten_array([123[123,['sad'['asd']]]]),'Incorrect data entered', "Test8 Fails");
+assert.deepStrictEqual(flatten_array(['ada',32,4,2,['asd',3]]),['ada',32,4,2,'asd',3], "Test9 Fails");
+assert.deepStrictEqual(flatten_array('ada'),'This function is for flattening array not any other datatype', "Test10 Fails");
